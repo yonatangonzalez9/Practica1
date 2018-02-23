1958ec6c2f22cdffec4083201261f6f389b21329
YonatanGB <yonatan.gonzalez@udea.edu.co>
2018-02-23 11:19:19 -0500

    Ejercicio 2

---
+++ Ejercicio2/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numero;
+    cout << "Escriba un numero: " << endl;
+    cin>> numero;
+
+    cout << "El numero que escribio es: " <<numero<< endl;
+
+    if(numero%2==0){
+        cout << "El numero es par"<<endl;
+
+        }
+    else{
+            cout<<"El numero es impar"<<endl;
+    }
+}
